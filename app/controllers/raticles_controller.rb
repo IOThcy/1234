31eade4ea171a34bac19735a8bb42c7d68b7ad4a
IOT_hcy <15708437379@163.com>
2015-07-24 11:04:37 +0800

    init

---
+++ app/controllers/raticles_controller.rb
@@ -0,0 +1,2 @@
+class RaticlesController < ApplicationController
+end
---
+++ test/controllers/raticles_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RaticlesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
